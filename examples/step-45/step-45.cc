8b231f433a725601d5827dcdf533935cbebb977f
Quang Hoang <qxhmorrow@gmail.com>
2023-06-01 12:43:14 -0400

    resolve hanging periodic boundary

--- examples/step-45/step-45.cc
+++ examples/step-45/step-45.cc
@@ -83,6 +83,7 @@ namespace Step45
     void solve();
     void output_results(const unsigned int refinement_cycle) const;
     void refine_mesh();
+    void resolve_hanging_on_periodic_boundary();
 
     const unsigned int degree;
 
@@ -718,7 +719,7 @@ namespace Step45
     //                          data_component_interpretation);
     Vector<float> subdomain(triangulation.n_active_cells());
     for (unsigned int i = 0; i < subdomain.size(); ++i)
-      subdomain(i) = triangulation.locally_owned_subdomain();
+      subdomain(i) =8;
     data_out.add_data_vector(subdomain, "subdomain");
     data_out.build_patches(mapping,0);
 
@@ -726,7 +727,29 @@ namespace Step45
       "./", "sol", refinement_cycle, MPI_COMM_WORLD, 2);
   }
 
+  template<int dim>
+  void StokesProblem<dim>::resolve_hanging_on_periodic_boundary(){
+    // output_results(1);
 
+    for(auto& cell: triangulation.active_cell_iterators()){
+      cell->clear_refine_flag();
+    }
+    for(auto &cell: triangulation.active_cell_iterators()){
+      if(cell->at_boundary()){
+        for(unsigned int i=0;i<cell->n_faces();++i){
+          if(cell->has_periodic_neighbor(i)){
+            if(cell->periodic_neighbor_is_coarser(i)){
+              TriaIterator<CellAccessor<dim,dim>> neighbor=cell->periodic_neighbor(i);
+              neighbor->set_refine_flag();
+            }
+          }
+        }
+      }
+    }
+    triangulation.execute_coarsening_and_refinement();
+    // output_results(2);
+    
+  }
 
   template <int dim>
   void StokesProblem<dim>::refine_mesh()
@@ -740,22 +763,25 @@ namespace Step45
       cell->set_refine_flag();
 
     }
-    for (auto &cell :
-       triangulation.active_cell_iterators() | refinement_subdomain_predicate){
-      if(cell->at_boundary()){
-        for(unsigned int i=0;i<cell->n_faces();++i){
-          if(cell->has_periodic_neighbor(i)){
+    // for (auto &cell :
+    //    triangulation.active_cell_iterators() | refinement_subdomain_predicate){
+    //   if(cell->at_boundary()){
+    //     for(unsigned int i=0;i<cell->n_faces();++i){
+    //       if(cell->has_periodic_neighbor(i)){
             
-            TriaIterator< CellAccessor< dim,dim > >  neighbor=cell->periodic_neighbor(i);
-            neighbor->set_refine_flag();
-          }
-        }
-      }
-    }
+    //         TriaIterator< CellAccessor< dim,dim > >  neighbor=cell->periodic_neighbor(i);
+    //         neighbor->set_refine_flag();
+    //       }
+    //     }
+    //   }
+    // }
 
     triangulation.execute_coarsening_and_refinement();
-  }
+    // StokesProblem<dim>::resolve_hanging_on_periodic_boundary();
+    
 
+  }
+  
 
   template <int dim>
   void StokesProblem<dim>::run()
@@ -763,7 +789,7 @@ namespace Step45
     create_mesh();
     output_results(0);
 
-    for (unsigned int refinement_cycle = 1; refinement_cycle < 2;
+    for (unsigned int refinement_cycle = 1; refinement_cycle < 3;
          ++refinement_cycle)
       {
         pcout << "Refinement cycle " << refinement_cycle << std::endl;
@@ -771,7 +797,7 @@ namespace Step45
         if (refinement_cycle > 0)
           refine_mesh();
 
-        // setup_dofs();
+        setup_dofs();
 
         // pcout << "   Assembling..." << std::endl << std::flush;
         // assemble_system();
