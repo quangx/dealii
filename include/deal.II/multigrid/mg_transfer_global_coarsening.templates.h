834036b694274b5cfd0c16187a7e19f03235facc
Marco Feder <marco.feder@sissa.it>
2023-08-20 17:12:47 +0000

    Expose rpe parameters through AdditionalData

--- include/deal.II/multigrid/mg_transfer_global_coarsening.templates.h
+++ include/deal.II/multigrid/mg_transfer_global_coarsening.templates.h
@@ -4738,6 +4738,18 @@ namespace internal
 } // namespace internal
 
 
+
+template <int dim, typename Number>
+MGTwoLevelTransferNonNested<dim, LinearAlgebra::distributed::Vector<Number>>::
+  MGTwoLevelTransferNonNested(const AdditionalData &data)
+{
+  rpe = std::make_shared<Utilities::MPI::RemotePointEvaluation<dim>>(
+    data.tolerance,
+    data.enforce_unique_mapping,
+    data.rtree_level,
+    data.marked_vertices);
+}
+
 template <int dim, typename Number>
 void
 MGTwoLevelTransferNonNested<dim, LinearAlgebra::distributed::Vector<Number>>::
@@ -4808,13 +4820,13 @@ MGTwoLevelTransferNonNested<dim, LinearAlgebra::distributed::Vector<Number>>::
       this->partitioner_fine->global_to_local(global_dof_indices[i]);
 
   // hand points over to RPE
-  rpe.reinit(points, dof_handler_coarse.get_triangulation(), mapping_coarse);
+  rpe->reinit(points, dof_handler_coarse.get_triangulation(), mapping_coarse);
 
   // set up MappingInfo for easier data access
-  mapping_info = internal::fill_mapping_info<dim, Number>(rpe);
+  mapping_info = internal::fill_mapping_info<dim, Number>(*rpe);
 
   // set up constraints
-  const auto &cell_data = rpe.get_cell_data();
+  const auto &cell_data = rpe->get_cell_data();
 
   constraint_info.reinit(dof_handler_coarse,
                          cell_data.cells.size(),
@@ -4823,7 +4835,7 @@ MGTwoLevelTransferNonNested<dim, LinearAlgebra::distributed::Vector<Number>>::
   for (unsigned int i = 0; i < cell_data.cells.size(); ++i)
     {
       typename DoFHandler<dim>::active_cell_iterator cell(
-        &rpe.get_triangulation(),
+        &rpe->get_triangulation(),
         cell_data.cells[i].first,
         cell_data.cells[i].second,
         &dof_handler_coarse);
@@ -4933,18 +4945,18 @@ MGTwoLevelTransferNonNested<dim, LinearAlgebra::distributed::Vector<Number>>::
       }
   };
 
-  rpe.template evaluate_and_process<value_type>(evaluation_point_results,
-                                                buffer,
-                                                evaluation_function);
+  rpe->template evaluate_and_process<value_type>(evaluation_point_results,
+                                                 buffer,
+                                                 evaluation_function);
 
   // Weight operator in case some points are owned by multiple cells.
-  if (rpe.is_map_unique() == false)
+  if (rpe->is_map_unique() == false)
     {
       const auto evaluation_point_results_temp = evaluation_point_results;
       evaluation_point_results.clear();
-      evaluation_point_results.reserve(rpe.get_point_ptrs().size() - 1);
+      evaluation_point_results.reserve(rpe->get_point_ptrs().size() - 1);
 
-      const auto &ptr = rpe.get_point_ptrs();
+      const auto &ptr = rpe->get_point_ptrs();
 
       for (unsigned int i = 0; i < ptr.size() - 1; ++i)
         {
@@ -5026,7 +5038,7 @@ MGTwoLevelTransferNonNested<dim, LinearAlgebra::distributed::Vector<Number>>::
   std::vector<value_type> evaluation_point_results;
   std::vector<value_type> buffer;
 
-  evaluation_point_results.resize(rpe.get_point_ptrs().size() - 1);
+  evaluation_point_results.resize(rpe->get_point_ptrs().size() - 1);
 
   for (unsigned int j = 0; j < evaluation_point_results.size(); ++j)
     {
@@ -5061,9 +5073,9 @@ MGTwoLevelTransferNonNested<dim, LinearAlgebra::distributed::Vector<Number>>::
     }
 
   // Weight operator in case some points are owned by multiple cells.
-  if (rpe.is_map_unique() == false)
+  if (rpe->is_map_unique() == false)
     {
-      const auto &ptr = rpe.get_point_ptrs();
+      const auto &ptr = rpe->get_point_ptrs();
 
       for (unsigned int i = 0; i < ptr.size() - 1; ++i)
         {
@@ -5108,9 +5120,9 @@ MGTwoLevelTransferNonNested<dim, LinearAlgebra::distributed::Vector<Number>>::
       }
   };
 
-  rpe.template process_and_evaluate<value_type>(evaluation_point_results,
-                                                buffer,
-                                                evaluation_function);
+  rpe->template process_and_evaluate<value_type>(evaluation_point_results,
+                                                 buffer,
+                                                 evaluation_function);
 }
 
 
